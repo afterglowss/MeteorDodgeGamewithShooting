5a4d94141fb0ded31590e86d2f037b33d760b7a5
LWG2311 <lug7531@naver.com>
2025-05-09 18:13:23 +0900

    feat: 플레이어 이동 구현 (#7)

--- MeteorDodgeGamewithShooting/player.h
+++ MeteorDodgeGamewithShooting/player.h
@@ -1,7 +1,23 @@
 #pragma once
+
 #define PLAYER_SIZE 20
 #define PLAYER_ACCEL 0.5f
 #define PLAYER_FRICTION 0.9f
 #define TRAIL_LENGTH 20
 #define ROTATE_LERP_FACTOR 0.15f
 #define INVINCIBLE_TIME 120
+#include "raylib.h"
+#include "raymath.h"
+
+struct Player
+{
+	Vector2 position;
+	Vector2 velocity;
+	float size;
+	int lives;
+}typedef Player;
+
+void InitPlayer();
+void UpdatePlayer();
+void DrawPlayer();
+
