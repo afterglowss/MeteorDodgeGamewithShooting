65cc210f303007a52e21626c3fe752ca69704d6e
o0Orangee <maboongee@gmail.com>
2025-05-12 22:31:49 +0900

    feat: 총알 생성 및 그리기 (#13)

--- MeteorDodgeGamewithShooting/bullet.h
+++ MeteorDodgeGamewithShooting/bullet.h
@@ -1,5 +1,17 @@
-#pragma once
+#include <stdlib.h>
+#include "raylib.h"
+
 #define MAX_BULLETS 50
-#define BULLET_SPEED 8
+#define BULLET_SPEED 1
 #define BULLET_RADIUS 4
 #define SHOOT_COOLDOWN_FRAMES 12
+
+typedef struct Bullet {
+	Vector2 position;
+	Vector2 velocity;
+	bool active;
+} Bullet;
+
+void FireBullet();
+void UpdateBullets();
+void DrawBullets();
