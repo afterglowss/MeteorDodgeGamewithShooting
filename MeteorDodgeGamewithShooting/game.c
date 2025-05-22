aa6840e32769802ddcb3c75ea8e8fc568ae3818d
pieuniverse <woojiwon0610@gmail.com>
2025-05-22 15:21:54 +0900

    fix: Game Over 백그라운드 사이즈 수정

--- MeteorDodgeGamewithShooting/game.c
+++ MeteorDodgeGamewithShooting/game.c
@@ -53,7 +53,7 @@ void DrawUI(Player player, int score, bool gameOver, bool gameStarted, int selec
         DrawText(TextFormat("Score: %d", score), 10, 35, 20, YELLOW);
 
         if (gameOver) {
-            DrawRectangle(0, 0, 800, 600, Fade(BLACK, 0.75f));
+            DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, Fade(BLACK, 0.75f));
             DrawText("GAME OVER", 460, 360, 40, RED);
             DrawText("Press ENTER to restart", 450, 410, 20, LIGHTGRAY);
             DrawText("Press BACKSPACE to Main", 445, 440, 20, LIGHTGRAY);
